0dca277dbb51b783d23b52a6cc3bd98bfeee5bdc
Oksana-Pak <okspak7@gmail.com>
2023-02-04 18:42:44 +0200

    change import

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,17 +1,22 @@
 import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { fetchMoviesQuery } from 'Api';
-import { MoviesList } from '../../components/MoviesList/MoviesList';
+import { MoviesList } from 'components/MoviesList/MoviesList';
 import { Input } from './Movies.styled';
 
 const Movies = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const query = searchParams.get('query');
+  const query = searchParams.get('query') ?? '';
   const [searchMovies, setSearchMovies] = useState(null);
 
   const onSubmit = e => {
     e.preventDefault();
-    setSearchParams({ query: e.currentTarget.elements.query.value.trim() });
+    const query = e.currentTarget.query.value.trim().toLowerCase();
+    if (!query) {
+      return;
+    }
+
+    setSearchParams({ query });
 
     e.currentTarget.reset();
   };
@@ -26,7 +31,7 @@ const Movies = () => {
   return (
     <>
       <form onSubmit={onSubmit}>
-        <Input type="text" name="query" />
+        <Input type="text" name="query" defaultValue={query} />
         <button type="submit">Search</button>
       </form>
       {searchMovies && <MoviesList movies={searchMovies} />}
